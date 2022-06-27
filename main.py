d20970a377f3a0b0e29617433dd31bdbe3e74ac4
Oluwatobi <sam16.oluwatobi@gmail.com>
2022-06-27 17:51:12 +0100

    ReadMe

--- main.py
+++ main.py
@@ -2,33 +2,43 @@ from tkinter import *
 import datetime
 from tkinter.messagebox import *
 from tkinter.ttk import *
-import winsound
+from playsound import playsound
 
+FONT_NAME = "Courier"
 window = Tk()
 window.title("Alarm clock by sammiie.com")
-window.config(padx=20, pady=50, bg='#29454f')
-window.geometry("450x300")
+window.config(padx=20, pady=50, bg='#2C3639')
+window.geometry("500x300")
 
 
 def alarm():
-    if period.get() == "AM":
-        x = int(hour_entry.get())
-        y = int(minute_entry.get())
-    if period.get() == "PM":
-        x = int(hour_entry.get())+12
-        y = int(minute_entry.get())
-    showinfo("notification", "alarm has been set")
-    while True:
-        if x == datetime.datetime.now().hour and y == datetime.datetime.now().minute:
-            for j in range(0,100):
-                winsound.Beep(1000, 100)
-            break
+    if not period.get():    # Check if a value has been selected for AM or PM
+        showerror("Error", "Kindly indicate if it's AM or PM")
+    else:
+        try:
+            if period.get() == "AM":
+                alarm_hour = int(hour_entry.get())
+                alarm_minute = int(minute_entry.get())
+            if period.get() == "PM":
+                alarm_hour = int(hour_entry.get())+12
+                alarm_minute = int(minute_entry.get())
+        except ValueError:
+            showinfo("info", "Invalid time entered!")
+        else:
+            showinfo("notification", "alarm has been set")
+
+            while True:
+                if alarm_hour == datetime.datetime.now().hour and alarm_minute == datetime.datetime.now().minute:
+                    playsound('audio.mp3')
+                    # for _ in range(0, 100):
+                    #     winsound.Beep(1000, 100)
+                    break
 
 # **************************************** USER INTERFACE DESIGN ****************************************************************
 
 
-hour_label = Label(window, text = "HOURS:")
-minute_label = Label(window, text= "MINUTES:")
+hour_label = Label(window, text = "HOURS:", font=(FONT_NAME, 15, "bold"))
+minute_label = Label(window, text= "MINUTES:", font=(FONT_NAME, 15, "bold"))
 
 hour_label.grid(row=0, column=0)
 minute_label.grid(row=0, column=2, padx=10)
@@ -39,7 +49,7 @@ minute_entry = Entry(window)
 hour_entry.grid(row=0, column=1, padx=10)
 minute_entry.grid(row=0, column=3)
 
-period_label = Label(window,text="AM OR PM:")
+period_label = Label(window,text="AM OR PM:", font=(FONT_NAME, 9, "bold"))
 period_label.place(relx=0.25,rely=0.31)
 period = Combobox(window, values=["AM", "PM"])
 period.place(relx=0.42, rely=0.3)
